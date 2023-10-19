9dc1671169d79a49118796a0f40fcd969233c77c
khanjancentennial <khanjancentennial@gmail.com>
2023-10-18 22:48:42 -0400

    add additional comments

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 
-// connect mongodb atlas with code using url
+// connect mongodb atlas with code using url to intereact with the server database
 module.exports =
 {
     "URI":"mongodb+srv://khanjancentennial:OzXcyEcDGGWNREGI@cluster0.4nxnnc0.mongodb.net/test?retryWrites=true&w=majority"
